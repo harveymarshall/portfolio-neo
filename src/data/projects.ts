edaaf1a4697906471be830c15b1b44ed87634b6b
Harvey Marshall <harvey.marshall@starlinksglobal.co.uk>
2026-05-21 14:38:13 +0100

    removed unused images, added larger icons for projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,25 +1,32 @@
+import {
+  type IconType,
+  SiApacheairflow,
+  SiAzuredevops,
+  SiSqlite,
+} from "@icons-pack/react-simple-icons"
+
 const PROJECTS: {
   name: string
   description: string
-  previewImage: string
+  project_icon: IconType
   repoUrl: string
 }[] = [
     {
       name: 'Airflow Dag Factory',
       description: 'A project built to obfuscate python code away from building apache airflow dags. This tool allows pipelines to be built using YAML config files. Those files are then used to build and deploy the pipelines to an apache airflow instance.',
-      previewImage: '/project1.png',
+      project_icon: SiApacheairflow,
       repoUrl: 'https://github.com/harveymarshall/airflow-dag-factory',
     },
     {
       name: 'Azure Blob Storage MCP Server',
       description: 'This project provides an MCP server for interacting with Azure Blob Storage using the MCP protocol. You can connect this server to clients like Claude Desktop for natural language access to your Azure Blob Storage resources.',
-      previewImage: '/project2.png',
+      project_icon: SiAzuredevops,
       repoUrl: 'https://github.com/harveymarshall/azure-blob-storage-mcp-server',
     },
     {
       name: 'Data Cleaning Agent',
       description: 'AI Agent that takes a raw dataset and recommends steps for cleaning the data based on User context provided to the Agent.',
-      previewImage: '/project3.png',
+      project_icon: SiSqlite,
       repoUrl: 'https://github.com/harveymarshall/data_cleaning_agent',
     },
   ]
